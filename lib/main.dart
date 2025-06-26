7db619cb9286753c32efaee8d0c995a7fdcf00d9
John Ryan <ryjohn@google.com>
2025-06-26 10:18:42 -0700

    Set up FFI code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
+import 'dart:ffi';
+import 'package:ffi_exp/avf_audio_bindings.dart';
+import 'package:objective_c/objective_c.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
+const _dylibPath =
+    '/System/Library/Frameworks/AVFAudio.framework/Versions/Current/AVFAudio';
+
+void main() async {
+  var args = ['test.mp3'];
+  DynamicLibrary.open(_dylibPath);
+  for (final file in args) {
+    final fileStr = NSString(file);
+    print('Loading $fileStr');
+    final fileUrl = NSURL.fileURLWithPath(fileStr);
+    final player = AVAudioPlayer.alloc().initWithContentsOfURL(
+      fileUrl,
+      error: nullptr,
+    );
+    if (player == null) {
+      throw('AVAudioPlayer failed to initialize');
+    }
+    final durationSeconds = player.duration.ceil();
+    print('$durationSeconds sec');
+    final status = player.play();
+    if (status) {
+      print('Playing...');
+      await Future<void>.delayed(Duration(seconds: durationSeconds));
+    } else {
+      print('Failed to play audio.');
+    }
+  }
   runApp(const MainApp());
 }
 
@@ -10,11 +40,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
