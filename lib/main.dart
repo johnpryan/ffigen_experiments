da17dde50602e0fc3fee6c0149da95f057688980
John Ryan <ryjohn@google.com>
2025-08-06 13:00:01 -0700

    use initFromData instead of initWithContentsOfURL

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'package:ffi_exp/avf_audio_bindings.dart';
+import 'package:flutter/services.dart';
 import 'package:objective_c/objective_c.dart';
 
 import 'package:flutter/material.dart';
@@ -10,34 +11,26 @@ const _foundationDylibPath =
     '/System/Library/Frameworks/Foundation.framework/Foundation';
 
 void main() async {
-  var args = ['assets/test.mp3'];
+  WidgetsFlutterBinding.ensureInitialized();
+  var fileData = await loadAsset('assets/test.mp3');
+  var uint8Data = fileData.buffer.asUint8List();
   DynamicLibrary.open(_avfAudioDylibPath);
-  DynamicLibrary.open(_foundationDylibPath);
-  final bundle = NSBundle.getMainBundle();
 
-  final resourcePath = bundle.resourcePath;
-  print('resourcePath = ${resourcePath!.toDartString()}');
-
-  for (final file in args) {
-    final fileStr = NSString('${resourcePath.toDartString()}/flutter_assets/test.mp3');
-    print('Loading ${fileStr.toDartString()}');
-    final fileUrl = NSURL.fileURLWithPath(fileStr);
-    final player = AVAudioPlayer.alloc().initWithContentsOfURL(
-      fileUrl,
-      error: nullptr,
-    );
-    if (player == null) {
-      throw('AVAudioPlayer failed to initialize');
-    }
-    final durationSeconds = player.duration.ceil();
-    print('$durationSeconds sec');
-    final status = player.play();
-    if (status) {
-      print('Playing...');
-      await Future<void>.delayed(Duration(seconds: durationSeconds));
-    } else {
-      print('Failed to play audio.');
-    }
+  final player = AVAudioPlayer.alloc().initWithData(
+    uint8Data.toNSData(),
+    error: nullptr,
+  );
+  if (player == null) {
+    throw ('AVAudioPlayer failed to initialize');
+  }
+  final durationSeconds = player.duration.ceil();
+  print('$durationSeconds sec');
+  final status = player.play();
+  if (status) {
+    print('Playing...');
+    await Future<void>.delayed(Duration(seconds: durationSeconds));
+  } else {
+    print('Failed to play audio.');
   }
   runApp(const MainApp());
 }
@@ -51,4 +44,8 @@ class MainApp extends StatelessWidget {
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
+}
+
+Future<ByteData> loadAsset(String path) async {
+  return await rootBundle.load(path);
 }
